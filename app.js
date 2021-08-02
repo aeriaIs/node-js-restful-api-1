eccc2b416458131235da473604bb4351801f99ad
Yudho Alfantyo <nosvengeance@gmail.com>
2021-08-02 09:33:52 +0700

    Error handling for unregistered link

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/cars', carRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found!');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message,
+    }
+  });
+})
+
 module.exports = app;
